2a49d51e619f466ddb2e3919518e2afe84ec1d31
Vadim-3 <128088822+Vadim-3@users.noreply.github.com>
2023-06-21 13:03:43 +0300

    fc

---
+++ virtual_assistant_v3.py
@@ -0,0 +1,123 @@
+from collections import UserDict
+from datetime import datetime, date
+
+class InvalidPhoneNumber(Exception):
+    pass
+
+class InvalidBirthday(Exception):
+    pass
+
+# батьківський клас для всіх полів 
+class Field:
+    def __init__(self, value=None):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def set_value(self, new_value):
+        self.value = new_value
+
+    def clear_value(self):
+        self.value = None
+
+    def is_empty(self):
+        return self.value is None
+
+
+# обов'язкове поле з ім'ям
+class Name(Field):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+# не обов'язкове поле з номером телефона
+class Phone(Field):
+    def __init__(self, phone):
+        super().__init__(phone)
+        
+class Birthday:
+    def __init__(self, day, month):
+        self.day = day
+        self.month = month
+
+
+# клас, який відповідає за логіку додавання/видалення/редагування необов'язкових полів та зберігання обов'язкового поля Name
+class Record:
+    def __init__(self, name, phone=None, birthday=None):
+        self.name = name
+        self.phones = []
+        self.birthday = self.validate_birthday(birthday)
+        if phone:
+            self.validate_phone(phone)
+
+
+    def validate_phone(self, phone):
+        if not isinstance(phone, str) or len(phone) != 10 or not phone.isdigit():
+            raise InvalidPhoneNumber("Invalid phone number")
+        return phone
+    
+    def validate_birthday(self, birthday):
+        if birthday is None:
+            return None
+
+        if not isinstance(birthday, Birthday):
+            raise InvalidBirthday("Invalid birthday")
+
+        return birthday
+    
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        if phone in self.phones:
+            self.phones.remove(phone)
+
+    def edit_phone(self, old_phone, new_phone):
+        if old_phone in self.phones:
+            index = self.phones.index(old_phone)
+            self.phones[index] = Phone(new_phone)
+
+    def get_phones(self):
+        return [phone.get_value() for phone in self.phones]
+
+    def days_to_birthday(self):
+        if self.birthday is None:
+            return None
+
+        current_date = datetime.date.today()
+        current_year = current_date.year
+
+        next_birthday = datetime.date(current_year, self.birthday.month, self.birthday.day)
+
+        if current_date > next_birthday:
+            next_birthday = datetime.date(current_year + 1, self.birthday.month, self.birthday.day)
+
+        days_until_birthday = (next_birthday - current_date).days
+        return days_until_birthday
+
+# наслідується від класу UserDict і виконує пошук за записами до цього класу
+class AddressBook(UserDict):
+    def __init__(self):
+        self.records = []
+
+    def add_record(self, record):
+        key = record.name.get_value()
+        self.data[key] = record
+        
+    def __iter__(self):
+        self.current_index = 0
+        return self
+
+    def __next__(self):
+        if self.current_index >= len(self.records):
+            raise StopIteration
+
+        page_records = self.records[self.current_index:self.current_index + self.page_size]
+        self.current_index += self.page_size
+
+        return page_records
+
+    def iterator(self, page_size=1):
+        self.page_size = page_size
+        return self
